50615d5ab251f8f5749be32dfffa5831b8bb5ce0
Ian Paschoal <104800595+IanZeta64@users.noreply.github.com>
2022-11-15 20:08:40 -0300

    Create Atv3_ex2.java

---
+++ Atv3_ex2.java
@@ -0,0 +1,25 @@
+import java.sql.SQLOutput;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Atv3_ex2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Integer idade;
+        try {
+            do {
+                System.out.println("Digite a idade do eleitor: ");
+                idade = sc.nextInt();
+                if (idade < 0) {
+                    System.out.println("valor da idade invalido!");
+                }
+            } while (idade < 0);
+
+            System.out.println((idade >= 18 && idade <= 70) ? "Voto obrigatório." :
+                    ((idade >= 16 && idade < 18) || idade > 70) ? "Voto facultativo." : "Sem direito a votar.");
+
+        }catch(InputMismatchException e) {
+            System.out.println("Erro de entrada de dados. Reinicie o programa.");
+        }
+    }
+}
